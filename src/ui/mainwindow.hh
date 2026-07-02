3263fbd4fea6476b69a04e07b1593892aa64d5cc
Wu Yun <wuyun1921@gmail.com>
2026-07-02 14:48:09 +0800

    feat: panel toggle shortcuts and menu items

--- src/ui/mainwindow.hh
+++ src/ui/mainwindow.hh
@@ -124,7 +124,8 @@ private:
 
   QAction escAction, focusTranslateLineAction, addTabAction, closeCurrentTabAction, closeAllTabAction,
     closeRestTabAction, switchToNextTabAction, switchToPrevTabAction, showDictBarNamesAction, toggleMenuBarAction,
-    lockPanelsAction, focusHeadwordsDlgAction, focusArticleViewAction, addAllTabToFavoritesAction;
+    lockPanelsAction, focusHeadwordsDlgAction, focusArticleViewAction, addAllTabToFavoritesAction,
+    togglePanelAction, togglePanelOrientationAction;
 
   QAction useSmallIconsInToolbarsAction, useLargeIconsInToolbarsAction, useNormalIconsInToolbarsAction;
 
